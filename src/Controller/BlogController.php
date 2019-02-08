3eb8e61a281d9e991b2da3b6336f058f48a68d85
ArckMax <maxime.gypteau@gmail.com>
2019-02-08 15:05:13 +0100

    Mise en place système d'authentification

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 /* Symfony injections */
 
+use App\Entity\Comment;
 use App\Form\ArticleType;
+use App\Form\CommentType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -81,11 +83,26 @@ class BlogController extends AbstractController
     /**
      * @Route("/blog/{id}", name="blog_show")
      */
-    public function show(Article $article)
+    public function show(Article $article, Request $request, ObjectManager $manager)
     {
+        $comment = new Comment();
+        $form = $this->createForm(CommentType::class, $comment);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment->setCreatedAt(new \DateTime())
+                ->setArticle($article);
+
+            $manager->persist($comment);
+            $manager->flush();
+
+            return $this->redirectToRoute("blog_show", ["id" => $article->getId()]);
+        }
 
         return $this->render("blog/show.html.twig", [
-            "article" => $article
+            "article" => $article,
+            "commentForm" => $form->createView()
         ]);
     }
 
